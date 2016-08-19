2a4ae8aa3756653031f50819b9ae722ffebd287a
liyibo [李一博] <liyibo@xiaomi.com>
2016-08-19 09:05:23 +0800

    example
    N/A

---
+++ example_test.go
@@ -0,0 +1,40 @@
+package inji
+
+import (
+	"fmt"
+	"miot/inji"
+)
+
+type Te struct {
+	Target int `inject:"target"`
+}
+
+func (t *Te) Start() error {
+	fmt.Println("start", t.Target)
+	return nil
+}
+
+func (t *Te) Close() {
+	fmt.Println("close", t.Target)
+}
+
+type Dep struct {
+	Test *Te `inject:"test"`
+}
+
+func (d *Dep) Close() {
+	fmt.Println("close Dep", d.Test)
+}
+
+func example() {
+	inji.InitDefault()
+	//test.Close, dep.Close will be called orderly
+	defer inji.Close()
+	inji.RegisterOrFail("target", 123)
+	//test will be auto created, test.Start will be called, then dep.Start
+	inji.RegisterOrFail("dep", (*Dep)(nil))
+	test, _ := inji.Find("test")
+	fmt.Println("find test", test)
+	dep, _ := inji.Find("dep")
+	fmt.Println("find dep", dep)
+}
